73d4dbd405f60860450c5a8f82a9d043fde82f19
wesfiala <weston.fiala@keysight.com>
2023-10-18 15:47:47 -0700

    Some files didn't get removed

--- main.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	get_tree().get_root().size_changed.connect(SettingsManager.trigger_reconfigure)
-
-
