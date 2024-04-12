4a5f6249a0bcac89edcd9503761eac89085c97c0
westonsfiala <westonsfiala@gmail.com>
2024-04-12 10:08:05 -0700

    Refactored some settings to flatten out the settings and make them act nicer

--- Scenes/SettingsPage/Scripts/enable_animations_setting.gd
+++ Scenes/SettingsPage/Scripts/enable_animations_setting.gd
@@ -1,42 +1,29 @@
 extends CollapsibleSettingBase
 class_name EnableAnimationsSetting
 
-@onready var inner_settings_container : VBoxContainer = $TopLevelContainer/CollapsibleContainer/CollapsibleSection/InnerSettingsContainer
-@onready var shake_volume_setting : ShakeVolumeSetting = $TopLevelContainer/CollapsibleContainer/CollapsibleSection/InnerSettingsContainer/ShakeVolumeSetting
+@onready var enable_button : Button = $TopLevelContainer/CollapsibleContainer/CollapsibleSection/EnableDisableContainer/EnableButton
+@onready var disable_button : Button = $TopLevelContainer/CollapsibleContainer/CollapsibleSection/EnableDisableContainer/DisableButton
+@onready var button_container : HBoxContainer = $TopLevelContainer/CollapsibleContainer/CollapsibleSection/EnableDisableContainer
 
 const ANIMATIONS_LABEL_TEXT : String = "Animations - "
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super()
-	# The default is collapsed, so expand if we are enabled.
-	var animations_enabled = SettingsManager.get_animations_enabled()
-	if animations_enabled:
-		start_expanded()
-		call_deferred("start_expanded")
-	inner_settings_container.minimum_size_changed.connect(setting_size_responder)
-
-# Calls the size responder deferred which is needed when setting size directly
-func setting_size_responder():
-	call_deferred("deferred_size_responder")
-
-# Set the size of the collapsible section 
-func deferred_size_responder():
-	#var new_size = inner_settings_container.size.y
-	#shake_volume_setting.size.y = shake_volume_setting.custom_minimum_size.y
-	#collapsible_section.custom_minimum_size.y = new_size
-	#collapsible_section.size.y = new_size
-	collapsible_section.custom_minimum_size.y = inner_settings_container.size.y
-	enforce_all_content_shown()
+	SettingsManager.animations_enabled_changed.connect(reconfigure_buttons)
+	reconfigure_buttons()
 	
-# Overwrite this method to enable/disable as we press it.
-func expand_collapse_inner_settings():
-	var animations_enabled = SettingsManager.get_animations_enabled()
-	SettingsManager.set_animations_enabled(not animations_enabled)
-	set_title()
+# Make sure the buttons are nice and visible.
+func reconfigure_buttons():
+	if SettingsManager.get_animations_enabled():
+		enable_button.button_pressed = true
+		disable_button.button_pressed = false
+	else:
+		enable_button.button_pressed = false
+		disable_button.button_pressed = true
+	
 	show_hide_reset_button()
-	emit_signal("setting_changed")
-	super()
+	set_title()
 	
 func inner_get_title() -> String:
 	var animations_enabled_string = "Disabled"
@@ -46,7 +33,7 @@ func inner_get_title() -> String:
 
 # Method for inherited class to get the minimum height of the collapsible section
 func inner_get_collapsible_section_minimum_height() -> int:
-	return int(inner_settings_container.size.y)
+	return int(button_container.size.y)
 	
 # Method for inherited class to implement if the reset button should be shown
 func inner_should_show_reset_button() -> bool:
@@ -54,14 +41,15 @@ func inner_should_show_reset_button() -> bool:
 
 # Method for inherited class to respond to reset being pressed
 func inner_reset_button_pressed():
-	if SettingsManager.get_animations_enabled() != SettingsManager.ANIMATIONS_ENABLED_DEFAULT:
-		if visible:
-			helper_expand_collapse_inner_settings(SettingsManager.ANIMATIONS_ENABLED_DEFAULT)
-		else:
-			start_expanded()
-		SettingsManager.set_animations_enabled(SettingsManager.ANIMATIONS_ENABLED_DEFAULT)
-		set_title()
-		show_hide_reset_button()
-		emit_signal("setting_changed")
+	SettingsManager.set_animations_enabled(SettingsManager.ANIMATIONS_ENABLED_DEFAULT)
+	emit_signal("setting_changed")
 
+# Set the animations enabled to true.
+func _on_enable_button_pressed():
+	SettingsManager.set_animations_enabled(true)
+	emit_signal("setting_changed")
 
+# Set the animations enabled to false.
+func _on_disable_button_pressed():
+	SettingsManager.set_animations_enabled(false)
+	emit_signal("setting_changed")
--- Scenes/SettingsPage/Scripts/top_level_roller_settings.gd
+++ Scenes/SettingsPage/Scripts/top_level_roller_settings.gd
@@ -3,21 +3,24 @@ class_name TopLevelRollerSettings
 
 @onready var settings_container : VBoxContainer = $TopLevelContainer/CollapsibleContainer/CollapsibleSection/SettingsContainer
 @onready var enable_animations_setting : EnableAnimationsSetting = $TopLevelContainer/CollapsibleContainer/CollapsibleSection/SettingsContainer/EnableAnimationsSetting
+@onready var shake_volume_setting : ShakeVolumeSetting = $TopLevelContainer/CollapsibleContainer/CollapsibleSection/SettingsContainer/ShakeVolumeSetting
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super()
 	enable_animations_setting.setting_changed.connect(show_hide_reset_button)
 	enable_animations_setting.reset_pressed.connect(show_hide_reset_button)
-	settings_container.minimum_size_changed.connect(setting_size_responder)
+	shake_volume_setting.setting_changed.connect(show_hide_reset_button)
+	shake_volume_setting.reset_pressed.connect(show_hide_reset_button)
+	settings_container.minimum_size_changed.connect(size_responder)
 
 # Calls the size responder deferred which is needed when setting size directly
-func setting_size_responder():
+func size_responder():
 	call_deferred("deferred_size_responder")
 
 # Set the size of the collapsible section 
 func deferred_size_responder():
-	var new_size = enable_animations_setting.size.y
+	var new_size = settings_container.size.y
 	collapsible_section.custom_minimum_size.y = new_size
 	enforce_all_content_shown()
 	
@@ -33,10 +36,12 @@ func inner_get_collapsible_section_minimum_height() -> int:
 # Method for inherited class to implement if the reset button should be shown
 func inner_should_show_reset_button() -> bool:
 	var should_show = false
-	#should_show = should_show or enable_animations_setting.inner_should_show_reset_button()
+	should_show = should_show or enable_animations_setting.inner_should_show_reset_button()
+	should_show = should_show or shake_volume_setting.inner_should_show_reset_button()
 	return should_show
 
 # Method for inherited class to respond to reset being pressed
 func inner_reset_button_pressed():
-	#enable_animations_setting.inner_reset_button_pressed()
+	enable_animations_setting.inner_reset_button_pressed()
+	shake_volume_setting.inner_reset_button_pressed()
 	show_hide_reset_button()
